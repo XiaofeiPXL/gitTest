dd8c02015658f2c262791c10e6e54114b9e4d392
xiaofei wang <xw8233@mun.ca>
2020-09-10 23:12:54 -0400

    init gitTest project

---
+++ src/main/java/cn/itcast/pojo/User.java
@@ -0,0 +1,12 @@
+package cn.itcast.pojo;
+
+/**
+ * @program: gitTest
+ * @description: User实体类
+ * @author: Xiaofei Wang
+ * @created: 2020/09/10 22:56
+ */
+
+
+public class User {
+}
---
+++ src/test/java/cn/itcast/test/dao/userDaoTest.java
@@ -0,0 +1,12 @@
+package cn.itcast.test.dao;
+
+/**
+ * @program: gitTest
+ * @description: UserDao测试类
+ * @author: Xiaofei Wang
+ * @created: 2020/09/10 22:56
+ */
+
+
+public class userDaoTest {
+}
